af6c0f5e0e76265065f35f7f8f68c30251729cca
Aaron Colaço <aaroncolaco.work@gmail.com>
2017-11-03 10:12:18 +0530

    Add Orderer & Peer component boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrdererComponent } from './components/orderer/orderer.component';
+import { PeerComponent } from './components/peer/peer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdererComponent,
+    PeerComponent
   ],
   imports: [
     BrowserModule
